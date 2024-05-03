31327695bcf8c2384dcc3c75897677ed01f0c011
gapls <125091918+G4PLS@users.noreply.github.com>
2024-05-03 13:40:52 +0200

    Fix(PulseEventListener): Forgot to add Event Listener making Plugin Unable to load

---
+++ internal/PulseEventListener.py
@@ -0,0 +1,23 @@
+import threading
+import pulsectl
+
+from src.backend.PluginManager.EventHolder import EventHolder
+
+
+class PulseEvent(EventHolder):
+
+    def __init__(self, plugin_base: "PluginBase", event_id: str):
+        super().__init__(plugin_base=plugin_base, event_id=event_id)
+        self.pulse_sink_thread = threading.Thread(target=self._start_loop)
+        self.pulse_sink_thread.daemon = True
+        self.pulse_sink_thread.start()
+
+    def _start_loop(self):
+        self._loop()
+
+    def _loop(self):
+        with pulsectl.Pulse("pulse-sink-event-loop") as pulse:
+            pulse.event_mask_set("sink")
+            pulse.event_callback_set(lambda event: self.trigger_event(event))
+            while True:
+                pulse.event_listen()
