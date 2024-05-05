2709ef7a38bee85e760c08298250cc5a6706fea6
gapls <125091918+G4PLS@users.noreply.github.com>
2024-05-05 17:03:01 +0200

    Feat(PulseEventListener): Added a way to specify the mask the listener should listen to

--- internal/PulseEventListener.py
+++ internal/PulseEventListener.py
@@ -6,18 +6,20 @@ from src.backend.PluginManager.EventHolder import EventHolder
 
 class PulseEvent(EventHolder):
 
-    def __init__(self, plugin_base: "PluginBase", event_id: str):
+    def __init__(self, plugin_base: "PluginBase", event_id: str, mask: str):
         super().__init__(plugin_base=plugin_base, event_id=event_id)
         self.pulse_sink_thread = threading.Thread(target=self._start_loop)
         self.pulse_sink_thread.daemon = True
         self.pulse_sink_thread.start()
+
+        self.mask = mask
 
     def _start_loop(self):
         self._loop()
 
     def _loop(self):
         with pulsectl.Pulse("pulse-sink-event-loop") as pulse:
-            pulse.event_mask_set("sink")
+            pulse.event_mask_set(self.mask)
             pulse.event_callback_set(lambda event: self.trigger_event(event))
             while True:
                 pulse.event_listen()
